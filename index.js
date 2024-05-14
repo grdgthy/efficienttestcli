bde3ca2b235ffe26b959be130c13f1ab2953c16a
grdgthy <osjcscarmeskell@hotmail.com>
2024-05-14 16:10:26 +0700

    update task

--- index.js
+++ index.js
@@ -1,14 +1,13 @@
-function productExceptSelf(nums) {
-  const result = [];
-  let product = 1;
-  for (let i = 0; i < nums.length; i++) {
-    result[i] = product;
-    product *= nums[i];
+function numDecodings(s) {
+  if (s.length === 0) return 0;
+  const dp = new Array(s.length + 1).fill(0);
+  dp[0] = 1;
+  dp[1] = s[0] !== "0" ? 1 : 0;
+  for (let i = 2; i <= s.length; i++) {
+    const oneDigit = parseInt(s.substring(i - 1, i));
+    const twoDigits = parseInt(s.substring(i - 2, i));
+    if (oneDigit >= 1) dp[i] += dp[i - 1];
+    if (twoDigits >= 10 && twoDigits <= 26) dp[i] += dp[i - 2];
   }
-  product = 1;
-  for (let i = nums.length - 1; i >= 0; i--) {
-    result[i] *= product;
-    product *= nums[i];
-  }
-  return result;
+  return dp[s.length];
 }
